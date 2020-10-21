7dfa43bf127ee00c51563dd33b0ea78700058eff
Darren Minsoo Kim <darren_minsoo_kim@outlook.com>
2020-10-21 10:29:00 -0400

    practicing

---
+++ 0002_add-two-numbers.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        def list_to_num(l):
+
+
+
+l1 = [2,4,3]
+r = list(reversed(l1))
